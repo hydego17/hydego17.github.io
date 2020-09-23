a50027269005d96e1df5c77b8db64fcf290b1b1b
Umma Ahimsha <uahimsha@gmail.com>
2020-09-23 14:16:11 +0700

    content update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
-const tl = gsap.timeline({defaults: {ease: 'power1.out'} });
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.to('.text', {y: "0%", duration: 1.5, stagger: 0.5, delay: 0.8});
+tl.to(".text", { y: "0%", duration: 0.75, stagger: 0.3, delay: 1.3 });
 
-tl.fromTo('#navbar', {opacity: 0 }, { opacity: 1, duration: 2}, '-=4');
-
+tl.fromTo("#navbar", { opacity: 0 }, { opacity: 1, duration: 2 }, "-=2");
