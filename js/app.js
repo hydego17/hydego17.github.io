9c7c5d307e7f32db2fba9f954012937259952cb4
Umma Ahimsha <uahimsha@gmail.com>
2020-10-16 07:13:56 +0700

    Add content

--- js/app.js
+++ js/app.js
@@ -3,3 +3,13 @@ const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 tl.to(".text", { y: "0%", duration: 0.75, stagger: 0.3, delay: 1.3 });
 
 tl.fromTo("#navbar", { opacity: 0 }, { opacity: 1, duration: 2 }, "-=2");
+
+gsap.from(".project-tile", {
+  scrollTrigger: {
+    trigger: ".project-tile",
+    scrub: 0.75,
+  },
+  y: 50,
+  scale: 1.05,
+  duration: 1.5,
+});
